6ec51e633eb5070bf357b06d75f88089458ab42c
soobinchoe <65845277+soobinchoe@users.noreply.github.com>
2022-08-19 16:08:51 +0800

    ADD delete at start & end

--- app/player_list.py
+++ app/player_list.py
@@ -6,12 +6,10 @@ class PlayerList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.is_empty_bool = True
 
     def insert_at_end(self, new_node: PlayerNode):
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-            return
+        self.is_empty(new_node)
 
         prev_node = self.tail
         prev_node.next = new_node
@@ -19,18 +17,39 @@ class PlayerList:
         self.tail = new_node
         self.tail.previous = prev_node
 
-    def insert_at_start(self, player_node: PlayerNode):
+    def insert_at_start(self, new_node: PlayerNode):
+        self.is_empty(new_node)
+
+        next_node = self.head
+        next_node.previous = new_node
+
+        self.head = new_node
+        self.head.next = next_node
+
+    def delete_at_start(self):
+        if self.is_empty_bool:
+            print("Nothing on the List")
+            return
+        prev_head = self.head
+        prev_head.next.previous = None
+        self.head = prev_head.next
+        prev_head.next = None
+
+    def delete_at_end(self):
+        if self.is_empty_bool:
+            print("Nothing on the List")
+            return
+        prev_tail = self.tail
+        prev_tail.previous.next = None
+        self.tail = prev_tail.previous
+        prev_tail.previous = None
+
+    def is_empty(self, player_node: PlayerNode):
         if not self.head:
             self.head = player_node
             self.tail = player_node
+            self.is_empty_bool = False
             return
-
-        next_node = self.head
-        next_node.previous = player_node
-
-        self.head = player_node
-        self.head.next = next_node
-
 
 
 if __name__ == "__main__":
@@ -41,5 +60,5 @@ if __name__ == "__main__":
     list1.insert_at_start(node)
     list1.insert_at_start(node2)
     list1.insert_at_start(node3)
-    print('hithere')
-    print(node3)
+    list1.delete_at_start()
+    print(list1.head)
--- test/player_list_test.py
+++ test/player_list_test.py
@@ -4,14 +4,16 @@ from app.player import Player
 from app.player_list import PlayerList
 from app.player_node import PlayerNode
 
-tuid1 = "123"
-tname1 = "soobin"
-tuid2 = "1234"
+tuid1 = "1"
+tname1 = "soobin1"
+tuid2 = "2"
 tname2 = "soobin2"
+tuid3 = "3"
+tname3 = "soobin3"
 
 
 class TestPlayer(unittest.TestCase):
-    def test_player_node_insert_at_start(self):
+    def test_insert_at_start(self):
         player1 = Player(tuid1, tname1)
         player2 = Player(tuid2, tname2)
         player_list = PlayerList()
@@ -21,7 +23,7 @@ class TestPlayer(unittest.TestCase):
         player_list.insert_at_start(player_node2)
         self.assertEqual(player_list.head, player_node2)
 
-    def test_player_node_insert_at_end(self):
+    def test_insert_at_end(self):
         player1 = Player(tuid1, tname1)
         player2 = Player(tuid2, tname2)
         player_list = PlayerList()
@@ -30,3 +32,27 @@ class TestPlayer(unittest.TestCase):
         player_list.insert_at_end(player_node1)
         player_list.insert_at_end(player_node2)
         self.assertEqual(player_list.tail, player_node2)
+
+    def test_delete_at_start(self):
+        player_list = PlayerList()
+        player_node1 = PlayerNode(Player(tuid1, tname1))
+        player_node2 = PlayerNode(Player(tuid2, tname2))
+        player_node3 = PlayerNode(Player(tuid3, tname3))
+        player_list.insert_at_end(player_node1)
+        player_list.insert_at_end(player_node2)
+        player_list.insert_at_end(player_node3)
+        player_list.delete_at_start()
+        self.assertEqual(player_list.head, player_node2)
+
+    def test_delete_at_end(self):
+        player_list = PlayerList()
+        player_node1 = PlayerNode(Player(tuid1, tname1))
+        player_node2 = PlayerNode(Player(tuid2, tname2))
+        player_node3 = PlayerNode(Player(tuid3, tname3))
+        player_list.insert_at_end(player_node1)
+        player_list.insert_at_end(player_node2)
+        player_list.insert_at_end(player_node3)
+        player_list.delete_at_end()
+        self.assertEqual(player_list.tail, player_node2)
+
+
